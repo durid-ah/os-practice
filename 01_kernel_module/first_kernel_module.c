3bfbd142e317260433ce38ebfeb5228e08f1fa31
Durid Ahmad <ahmad.durid96@gmail.com>
2022-05-23 14:24:11 -0600

    initial kernel values

---
+++ 01_kernel_module/first_kernel_module.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+// Call when loading module
+int simple_init(void) {
+   printk(KERN_INFO "Loading Module\n");
+}
+
+// Call when module is removed
+void simple_exit(void) {
+   printk(KERN_INFO "Loading Module\n");
+}
+
+// Register Entry and Exit
+module_init(simple_init);
+module_exit(simple_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODUEL_AUTHOR("SGG");
