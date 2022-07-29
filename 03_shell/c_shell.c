5b4f4ee95bfc132ccb39be9dcbb0cf48a24c526b
Durid Ahmad <ahmad.durid96@gmail.com>
2022-07-29 06:54:29 -0600

    cleanup and making the child execute the command

--- 03_shell/c_shell.c
+++ 03_shell/c_shell.c
@@ -2,13 +2,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 80 // Maximum length command
 
-int parse_input(char input[], char* args[]) {
+int parse_input(char input[], char command[], char* args[]) {
+    // remove trailing \n
     input[strcspn(input, "\n")] = 0;
-    // TODO: strtok_r? 
+    
+    // get the command
     char* arg = strtok(input, " ");
+
+    // get the other args
     int i = 0;
     while (arg != NULL) {
         args[i] = arg;
@@ -26,33 +31,30 @@ int main(void) {
     while (should_run) {
         pid_t pid;
         char str[MAX_LINE];
+        char command[MAX_LINE];
 
         printf("osh > ");
         fflush(stdout);
         fgets(str, MAX_LINE, stdin);
 
-        int arg_count = parse_input(str, args);
-
-        int y = 0;
-        while (y < arg_count) {
-            printf("ret: %s \n", args[y]);
-            y++;
-        }
-
+        int arg_count = parse_input(str, command, args);
 
         /**
          * fork
          * child invokes execvp()
          * if command included & parent will wait()
          */
-        // pid = fork();
-        // if (pid < 0) {
-        //     fprintf(stderr, "Fork Failes");
-        // } else if (pid == 0) {
-        //     // child
-        // } else {
-        //     // parent
-        // }
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "Fork Failed");
+        } else if (pid == 0) {
+            execvp(args[0], args);
+            fflush(stdout);
+            return 0;
+        } else {
+            wait(NULL);
+            // parent
+        }
 
     }
 }
