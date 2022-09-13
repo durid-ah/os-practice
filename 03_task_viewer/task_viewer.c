5cd55c3211ec8f04c4a022a74b4e8d9705281641
Durid Ahmad <ahmad.durid96@gmail.com>
2022-09-12 21:35:15 -0600

    task viewer stub

---
+++ 03_task_viewer/task_viewer.c
@@ -0,0 +1,24 @@
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+
+// Call when loading module
+int simple_init(void) {
+   printk(KERN_INFO "Loading Module\n");
+   return 0;
+}
+
+// Call when module is removed
+void simple_exit(void) {
+   printk(KERN_INFO "Remove Module\n");
+}
+
+// Register Entry and Exit
+module_init(simple_init);
+module_exit(simple_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
